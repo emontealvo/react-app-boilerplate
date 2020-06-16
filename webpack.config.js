5d9dd9c004c3c324c7ed3e05318874cc282495b8
emontealvo <edwin.montealvo@gmail.com>
2020-06-16 12:06:53 -0600

    Add css and sass packages and settings

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,10 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
+				use: ['babel-loader']
+			}, {
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	},
